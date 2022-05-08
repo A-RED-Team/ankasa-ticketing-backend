6c6a56a90e2429d441be70cba5f787c45b005873
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-05-09 06:18:43 +0700

    finish feature

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -5,7 +5,7 @@ module.exports = {
   APP_NAME: process.env.APP_NAME || 'Ankasa Ticketing',
   APP_VERSION: process.env.APP_VERSION || '1.0.0',
   APP_STATUS: process.env.APP_STATUS || 'development',
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 4000,
   // database
   PG_HOST: process.env.PG_HOST,
   PG_USER: process.env.PG_USER,
